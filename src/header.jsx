e3af0869c0131a9bdaf47e9c6918150dedb750a7
Oluwaseun2003 <oluwafemioluwaseun421@gmail.com>
2023-09-24 00:22:19 +0100

    1

---
+++ src/header.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import  ReactDOM  from "react-dom";
+
+function Header() {
+    <>
+     <header className="header">
+        <a href="#" className="logo">Oluwaseun</a>
+
+        <nav className="navbar">
+            <a href="#">Home</a>
+            <a href="#">About</a>
+            <a href="#">Education</a>
+            <a href="#">Skills</a>
+            <a href="#">Contact</a>
+        </nav>
+     </header>
+    </>
+}
