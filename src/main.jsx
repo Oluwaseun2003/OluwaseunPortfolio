f4c7156f3ee362eefcbef21e7c73ea4354fa865c
Oluwaseun2003 <oluwafemioluwaseun421@gmail.com>
2023-09-25 09:55:08 +0100

    Commit

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import Header from './header.jsx'
 import Homepage from './Homepage.jsx'
 import Aboutpage from './About.jsx'
 import Education from './Education.jsx'
+import Skills from './Skills.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
@@ -12,6 +13,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <Header />,
     <Homepage />,
     <Aboutpage />,
-    <Education />
+    <Education />,
+    <Skills />
   </>
 )
