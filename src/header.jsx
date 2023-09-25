ed48a1af704a5db6927254a1e122d280e6c72ced
Oluwaseun2003 <oluwafemioluwaseun421@gmail.com>
2023-09-25 17:23:37 +0100

    mm

--- src/header.jsx
+++ src/header.jsx
@@ -6,25 +6,28 @@ import './header.css'
 
 
 function Header() {
+    let menuIcon = document.querySelector("#menu-icon")
+    let navbar = document.querySelector("navbar")
 
     let sections = document.querySelectorAll('section')
     let navLinks = document.querySelectorAll('header nav a')
 
 
     window.onscroll = () =>{
-        sections.forEach(sec =>{
-            let top = window.scrollY;
-            let offset = sec.offsetTop - 100;
-            let height = sec.offsetHeight;
-            let id = sec.getAttribute('id')
+       sections.forEach(sec =>{
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 100;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id')
 
-            if(top >= offset && top < offset + height){
-                navLinks.forEach(links => {
-                    links.classList.remove('active');
-                    document.querySelector('header nav a[href*=' + id + ']').classList.add(active);
-                });
-            }
-        });
+        if(top >= offset && top < offset + height) {
+
+            navLinks.forEach(links =>{
+                links.classList.remove("active")
+                document.querySelector("header nav a[href*=" + id + "]").classList.add("active")
+            })
+        }
+       })
 
 
         let header = document.querySelector('header')
